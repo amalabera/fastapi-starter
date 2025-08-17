101eaa7dee482b45eebc0cb5200011216000b840
amalabera <BERAAMALA43@GMAIL.COM>
2025-08-17 11:33:29 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI, APIRouter
+app = FastAPI(title="fastapi-starter")
+router = APIRouter(prefix="/v1", tags=["v1"])
+
+@router.get("/echo")
+def echo(msg: str): return {"echo": msg}
+
+@app.get("/health")
+def health(): return {"status": "ok"}
+
+app.include_router(router)
 
